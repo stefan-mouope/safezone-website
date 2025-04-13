ab0dd08a333154bee58639bfffb5269f7111e3aa
stefan-mouope <stefan.bouognong@facsciences-uy1.cm>
2025-04-13 13:07:06 +0100

    modif

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from "../../assets/logo.png";
+import menuIcon from "../../assets/menu.png";
 import './Header.css';
 
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="header">
-     <div className="logo-container">
+      <div className="logo-container">
         <img src={logo} alt="SafeZone logo" className="logo-image" />
         <span className="logo-text">SafeZone</span>
       </div>
-      <nav className="nav"> 
-        <ul>
+
+      <img
+        src={menuIcon}
+        alt="Menu"
+        className="menu-icon"
+        onClick={toggleMenu}
+      />
+
+      <nav className={`nav ${isMobileMenuOpen ? "active" : ""}`}>
+        <ul onClick={() => setIsMobileMenuOpen(false)}>
           <li><a href="#home">Accueil</a></li>
           <li><a href="#about">À propos</a></li>
           <li><a href="#features">Fonctionnalités</a></li>
